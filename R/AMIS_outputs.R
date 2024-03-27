00be9cf390c9ee627187394876387dff5b225227
Evandro Konzen <evankonzen@gmail.com>
2024-03-27 13:02:01 +0000

    show parameter names in grids of pairwise plots for models with d>=3 parameters

--- R/AMIS_outputs.R
+++ R/AMIS_outputs.R
@@ -460,12 +460,11 @@ plot_mixture_components <- function(x, what="uncertainty", iteration=NULL,
     # ylab = colnames(x$param)[2]
     cat(paste0("Plotting density of the mixture model at iteration ", iteration, "...\n"))
     # mclust::plot.Mclust(clustering, what=what, xlab=xlab, ylab=ylab, ...)
+    colnames(clustering$data) <- colnames(x$param)
     if(d==1){
       mclust::plot.Mclust(clustering, what=what, xlab=param_names, xlim=xlim, ylim=ylim, ...)
-    }else if(d==2){
-      mclust::plot.Mclust(clustering, what=what, xlab=param_names[1], ylab=param_names[2], xlim=xlim, ylim=ylim, ...)
     }else{
-      mclust::plot.Mclust(clustering, what=what, xlim=xlim, ylim=ylim, ...)  
+      mclust::plot.Mclust(clustering, what=what, xlim=xlim, ylim=ylim, ...)
     }
     main <- ifelse(is.null(main), "Proposal Density", main)
     title(main = main)

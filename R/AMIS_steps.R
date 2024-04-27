a786eef18ae5b1dd9c1bac58101c94af789f330b
Evandro Konzen <evankonzen@gmail.com>
2024-04-27 15:44:07 +0100

    fix bug when num_locs=1 and Gaussian kernel is used

--- R/AMIS_steps.R
+++ R/AMIS_steps.R
@@ -194,8 +194,12 @@ compute_likelihood <- function(param,prevalence_map,simulated_prevalences,amis_p
   n_sims <- dim(simulated_prevalences)[1]
   lik <- array(NA, c(n_tims,n_locs,n_sims)) # this way around to avoid abind -- different to elsewhere
   for (t in 1:n_tims) {
+    log_norm_const_gaussian_t <- log_norm_const_gaussian[t,,]
+    if(n_locs==1){
+      log_norm_const_gaussian_t <- t(log_norm_const_gaussian_t)  
+    }
     lik[t,,] <- evaluate_likelihood(param,prevalence_map[[t]],simulated_prevalences[,t],amis_params,
-                                    which_valid_sim_prev_iter[[t]],which_valid_prev_map[[t]],log_norm_const_gaussian[t,,]) 
+                                    which_valid_sim_prev_iter[[t]],which_valid_prev_map[[t]],log_norm_const_gaussian_t) 
   }
   if (!is.null(likelihoods)) {
     lik <- array(c(likelihoods,lik), c(n_tims,n_locs,dim(likelihoods)[3]+n_sims))
